0fba37ba977f632274c4608528a2e62e3d93735d
chimg622 <chimg622@gmail.com>
2026-03-17 19:44:52 +0900

    店舗登録機能実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,21 @@
 class ShopsController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :address).merge(user_id: current_user.id)
+  end
 end
