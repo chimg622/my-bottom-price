f7d80c49e9ccacbc5365b9b10219d4664b4d394b
chimg622 <chimg622@gmail.com>
2026-04-03 17:16:21 +0900

    結合テストコードの記述

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -52,3 +52,49 @@ RSpec.describe 'ユーザー新規登録', type: :system do
     end
   end
 end
+
+RSpec.describe 'ユーザー編集機能', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ユーザー編集ができるとき' do
+    it 'ログインしたユーザーはユーザー編集ができる' do
+      # トップページに遷移する
+      visit "http://#{ENV['BASIC_AUTH_USER']}:#{ENV['BASIC_AUTH_PASSWORD']}@127.0.0.1:#{Capybara.current_session.server.port}#{root_path}"
+      # トップページにログインボタンがあることを確認
+      expect(page).to have_content('ログイン')
+      # ログインページへ移動する
+      visit new_user_session_path
+      # ユーザー情報を入力してログインする
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      find('input[name="commit"]').click
+      expect(page).to have_current_path(root_path)
+      # マイページに遷移する
+      visit user_path
+      # 編集ボタンが表示されることを確認する
+      expect(page).to have_content('プロフィールを編集')
+      # 編集ページに遷移する
+      visit edit_user_registration_path
+      # 新しい名前とメールアドレス、現在のパスワードを入力して、アカウント更新ボタンを押す
+      fill_in 'user_name', with: 'テストユーザー'
+      fill_in 'user_email', with: 'test@example.com'
+      fill_in 'user_password', with: 'password123'
+      fill_in 'user_password_confirmation', with: 'password123'
+      fill_in 'user_current_password', with: @user.password
+      find('input[name="commit"]').click
+      # トップページに遷移することを確認する
+      expect(page).to have_current_path(root_path)
+      # トップページに新しい名前が存在することを確認する
+      expect(page).to have_content('テストユーザー')
+    end
+  end
+  context 'ユーザー編集ができないとき' do
+    it 'ログインしていないユーザーはユーザー編集画面に遷移しようとするとログイン画面にリダイレクトされる' do
+      # 編集ページに遷移する
+      visit edit_user_registration_path
+      # ログインページに遷移することを確認する
+      expect(page).to have_current_path(new_user_session_path)
+    end
+  end
+end
