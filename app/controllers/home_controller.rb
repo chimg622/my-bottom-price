046be38752a4b757ee399783277d443834aab017
chimg622 <chimg622@gmail.com>
2026-04-16 11:29:17 +0900

    コード統一化、修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 # app/controllers/home_controller.rb
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
-    return unless user_signed_in?
-
     @recent_items = current_user.items.includes(:category).order(created_at: :desc).limit(3)
     @recent_prices = current_user.prices.includes(:item, :shop).order(created_at: :desc).limit(5)
   end
